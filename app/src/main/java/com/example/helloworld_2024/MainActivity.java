a378b5918d4e466cda05c5efa1f00dca8f03877e
Erzana <aeaaed5@gmail.com>
2024-01-25 12:35:25 -0500

    Initial commit

---
+++ app/src/main/java/com/example/helloworld_2024/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.helloworld_2024;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void btn_click(View v)
+    {
+        TextView tv_msg = findViewById(R.id.msg);
+        tv_msg.setText("Welcome!");
+        Toast.makeText(getApplicationContext(), "Hello Erzana", Toast.LENGTH_SHORT).show();
+    }
+}
